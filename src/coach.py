e04f9bad27a925e771ff1046eea8984f1990f34b
william-Silver-droid <107883193+william-Silver-droid@users.noreply.github.com>
2024-03-08 11:34:31 -0700

    Update coach.py

--- src/coach.py
+++ src/coach.py
@@ -2,4 +2,10 @@ import time as t
 import numpy as np
 #this is the coach class made to be the timer and score keeper
 class Coach:
+    typed # the character typed by the user
+    key # answer key the words the user is supposed to type
+    def __init__(self, key, typed=""):
+        self.typed = typed
+        self.key = key
+
   

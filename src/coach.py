0e0ca4ef55f4517f60bda7d0f1139bb88895a36d
william-Silver-droid <107883193+william-Silver-droid@users.noreply.github.com>
2024-03-11 10:31:13 -0600

    Update coach.py

--- src/coach.py
+++ src/coach.py
@@ -3,16 +3,20 @@ import numpy as np
 import nltk
 #this is the coach class made to be the timer and score keeper
 class coach:
-  def __init__(self, key, user_typed) -> None:
-    self.key = key # app input
-    self.user_typed = user_typed # user input
-def calc_acc(self):
-    oran_text = self.key.split()
-    user_text = self.user_typed.split()
-    user_text = user_text[0 :len(user_text)]
-    BLEUscore = nltk.translate.bleu_score.sentence_bleu(oran_text, user_text) #call on sentence comparison
-    Acc_score = BLEUscore*100
-    print(Acc_score)
-    return(Acc_score)
+    def __init__(self, key, user_typed) -> None:
+        self.key = key
+        self.user_typed = user_typed
+    def calc_acc(self):
+        oran_text = self.key.split()
+        user_text = self.user_typed.split()
+        oran_text = oran_text[0 :len(user_text)]
+        print("user_text", user_text)
+        print("oran_text", oran_text)
+        BLEUscore = nltk.translate.bleu_score.sentence_bleu(oran_text, user_text) 
+        
+        Acc_score = BLEUscore*100
+
+        print(Acc_score)
+        return(Acc_score)
 
   

73c201d58eeacf0127d3afb9ba7a018ddf5781e4
william-Silver-droid <107883193+william-Silver-droid@users.noreply.github.com>
2024-03-11 10:06:03 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,7 +5,6 @@ import time
 import csv
 import utils
 import text_generator as tg  # Importing custom module text_generator as tg
-import nltk #for BLEUscore
 
 def main():
     pygame.init()  # Initialize pygame
@@ -26,44 +25,79 @@ def main():
     text_font = pygame.font.SysFont("Times New Roman", 30)
     run = True
     separator = ""  # Variable to hold separator for joining strings
-    words = tg.generator(words=0, n_words=0)  # Creating an instance of text_generator
+    words = tg.generator(words=0, n_words=0, n=0)  # Creating an instance of text_generator
     words.load_words()  # Loading words
     words.generate_text()  # Generating text
 
     # Rendering text
-    text = text_font.render(":", True, black)
+    text = text_font.render(":", True, orange)
     current_string = []  # List to hold user input string
     screen.blit(text, (110, 110))
     screen.fill(gray)  # Filling the screen with a gray background
-
+    num_words = []
+    starting = True
     while run:
         # Drawing text on the screen
-        maybe = draw_text(words.type_words, text_font, orange, 0, 0, screen)
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RETURN:
-                    break
-                elif event.key == pygame.K_BACKSPACE:
-                    current_string = current_string[:-1]  # Removing the last character from the string
-                    screen.fill(white)  # Filling the screen with white color - prevents overlay
-                else:
-                    try:
-                        print(chr(event.key))
-                        current_string.append(chr(event.key))  # Appending the character to the string
-                    except:
-                        pass
-                typed = text_font.render(separator.join(current_string), True, black)  # Rendering typed text
-                print("typed", separator.join(current_string))
-                print("words", maybe)
-                screen.blit(typed, (0, 100))
-                print(separator.join(current_string) == maybe[:-1])
-                user_typed = separator.join(current_string)
-                if separator.join(current_string) == maybe[:-1]:  # Checking if typed text matches generated text
-                    print("here")
-                    pygame.quit()  # Quitting pygame
-            if event.type == pygame.QUIT:
-                run = False
-        pygame.display.flip()  # Updating the display
+        if starting == False:
+          maybe = draw_text(words.type_words, text_font, orange, 0, 0, screen)
+          for event in pygame.event.get():
+              if event.type == pygame.KEYDOWN:
+                  if event.key == pygame.K_RETURN:
+                      break
+                  elif event.key == pygame.K_BACKSPACE:
+                      current_string = current_string[:-1]  # Removing the last character from the string
+                      screen.fill(gray)  # Filling the screen with white color
+                  else:
+                      try:
+                          print(chr(event.key))
+                          current_string.append(chr(event.key))  # Appending the character to the string
+                      except:
+                          pass
+                  typed = text_font.render(separator.join(current_string), True, orange)  # Rendering typed text
+                  print("typed", separator.join(current_string))
+                  print("words", maybe)
+                  screen.blit(typed, (0, 100))
+                  print(separator.join(current_string) == maybe[:-1])
+                  user_typed = separator.join(current_string)
+                  if separator.join(current_string) == maybe[:-1]:  # Checking if typed text matches generated text
+                      print("here")
+                      pygame.quit()  # Quitting pygame
+              if event.type == pygame.QUIT:
+                  run = False
+          pygame.display.flip()  # Updating the display
+        else:
+          maybe = draw_text("How many words do you want to type", text_font, orange, 0, 0, screen)
+          for event in pygame.event.get():
+              if event.type == pygame.KEYDOWN:
+                  if event.key == pygame.K_RETURN:
+                      screen.fill(gray)
+                      starting = False
+                      n = num_words
+                      words = tg.generator(words=0, n_words=0, n=int(separator.join(n)))  # Creating an instance of text_generator
+                      words.load_words()  # Loading words
+                      words.generate_text()
+                      num_words = ""
+                  elif event.key == pygame.K_BACKSPACE:
+                      num_words = num_words[:-1]  # Removing the last character from the string
+                      screen.fill(gray)  # Filling the screen with white color
+                  else:
+                      try:
+                          print(chr(event.key))
+                          num_words.append(chr(event.key))  # Appending the character to the string
+                      except:
+                          pass
+                  typedw = text_font.render(separator.join(num_words), True, black)  # Rendering typed text
+                  print("typed", separator.join(num_words))
+                  print("words", maybe)
+                  screen.blit(typedw, (0, 100))
+                  print(separator.join(num_words) == maybe[:-1])
+                  user_typed = separator.join(num_words)
+                  if separator.join(num_words) == maybe[:-1]:  # Checking if typed text matches generated text
+                      print("here")
+                      pygame.quit()  # Quitting pygame
+              if event.type == pygame.QUIT:
+                  run = False
+          pygame.display.flip()  # Updating the display
 
     pygame.quit()  # Quitting pygame
     return 0

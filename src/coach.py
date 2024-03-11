9216668bcaaba3013c00c943f5820c05db4248dc
lyraeho <142616800+lyraeho@users.noreply.github.com>
2024-03-11 09:10:20 -0600

    Update coach.py

--- src/coach.py
+++ src/coach.py
@@ -1,11 +1,12 @@
 import time as t
 import numpy as np
+import nltk
 #this is the coach class made to be the timer and score keeper
-class Coach:
-    typed # the character typed by the user
-    key # answer key the words the user is supposed to type
-    def __init__(self, key, typed=""):
-        self.typed = typed
-        self.key = key
+class coach:
+  oran_text = key
+  user_text = user_typed
+  BLEUscore = nltk.translate.bleu_score.sentence_bleu([reference], hypothesis) 
+  Acc_score = Bleuscore*100
+  print(Acc_score)
 
   
